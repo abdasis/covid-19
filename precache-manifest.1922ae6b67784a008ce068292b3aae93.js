b03ae306081bb2a65f185cf84d2c015b4faeebf1
Abd. Asis <id.juniordeveloper@gmail.com>
2020-03-18 02:22:44 +0700

    Updates

---
+++ precache-manifest.1922ae6b67784a008ce068292b3aae93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fcaa369c861cfda966fdc7af5ba89b2",
+    "url": "/%7Bcovid-19%7D/index.html"
+  },
+  {
+    "revision": "ea0a326d83d754e6d685",
+    "url": "/%7Bcovid-19%7D/static/js/2.f447f45f.chunk.js"
+  },
+  {
+    "revision": "f961bb9f6dda31d42683aee99edc86b0",
+    "url": "/%7Bcovid-19%7D/static/js/2.f447f45f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82b932213046e74f3b3",
+    "url": "/%7Bcovid-19%7D/static/js/main.d9f79c1b.chunk.js"
+  },
+  {
+    "revision": "880c5cdcdbf95def6d40",
+    "url": "/%7Bcovid-19%7D/static/js/runtime-main.450c6096.js"
+  }
+]);
